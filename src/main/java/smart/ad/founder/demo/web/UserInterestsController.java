42cff277452e8e9d4bebed9f312df5bd49a2715b
Andrej Hristov <andrejhris23@gmail.com>
2020-12-20 20:09:37 +0100

    finished the UserInterests Controller in the web package

--- src/main/java/smart/ad/founder/demo/web/UserInterestsController.java
+++ src/main/java/smart/ad/founder/demo/web/UserInterestsController.java
@@ -1,4 +1,46 @@
 package smart.ad.founder.demo.web;
 
+import org.springframework.web.bind.annotation.*;
+import smart.ad.founder.demo.application.service.UserInterestsService;
+import smart.ad.founder.demo.domain.model.entities.UserInterest;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/userInterests")
+@CrossOrigin("*")
+
 public class UserInterestsController {
+
+    UserInterestsService userInterestsService;
+
+    public UserInterestsController(UserInterestsService userInterestsService) {
+        this.userInterestsService = userInterestsService;
+    }
+
+    @GetMapping("/all")
+    public List<UserInterest> getAllUserInterests() {
+        return userInterestsService.findAllUserInterests();
+    }
+
+    @GetMapping("/{id}")
+    public UserInterest getUserInterestById(@PathVariable("id") Long id) {
+        return userInterestsService.findUserInterestById(id);
+    }
+
+    @PatchMapping("/editUserInterest")
+    public UserInterest editUserInterest(@RequestBody UserInterest newUserInterest) {
+        return userInterestsService.editUserInterest(newUserInterest);
+    }
+
+    @PostMapping("/createUserInterest")
+    public UserInterest userInterest(@RequestBody UserInterest userInterest) {
+        return userInterestsService.editUserInterest(userInterest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserInterest(@PathParam("id") Long id) {
+        userInterestsService.deleteUserInterestById(id);
+    }
 }
